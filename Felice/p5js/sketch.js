38e589f1dae9162eecb70964f23f0ac7b1001c8a
felicechu <81840596+felicechu@users.noreply.github.com>
2021-04-07 18:53:04 -0400

    house

--- Felice/p5js/sketch.js
+++ Felice/p5js/sketch.js
@@ -1,10 +1,21 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(320);
-  circle(200,200,200);
-  circle(100,200,100);
-  circle(300,200,100);
+  background(400);
+  fill(244,170,169);
+  square(100, 200, 200);
+  fill(241,237,226);
+  triangle(60,200,340,200,200,75);
+  fill(159,148,136);
+  square(120,260,55);
+  square(220,260,55);
+  fill(241,237,226);
+  rect(170, 345, 55, 55)
+  line(220,290,275,290);
+  line(120,290,175,290);
+  line(247,260,247,315);
+  line(147,260,147,315);
+  circle(214,375,15)
 }
